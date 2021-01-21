7b2edc40b3c95208f791f387e251785f5810e4c7
Nick Anich <47158373+nmanich@users.noreply.github.com>
2021-01-20 22:41:11 -0600

    Update CreatingandPopulatingTablesInPostgreSQL.sql

--- CreatingandPopulatingTablesInPostgreSQL.sql
+++ CreatingandPopulatingTablesInPostgreSQL.sql
@@ -1,8 +1,8 @@
---this makes a database called dec6test
-CREATE DATABASE dec6test;
+--this makes a database called dec2020allportals
+CREATE DATABASE dec2020allportals;
 
---this command connects to the database called dec6test
-\c dec6test;
+--this command connects to the database called dec2020allportals
+\c dec2020allportals;
 
 /*  
 These are the structures of the table: column name, then data type, and constraints.  * Use the R code to split the eBird download into 4 csv files that will get loaded into  * these tables after they are created. 
@@ -10,6 +10,7 @@ These are the structures of the table: column name, then data type, and constrai
 
 -- this fixes some character encoding issues
 SET CLIENT_ENCODING TO 'utf8';
+
 
 CREATE TABLE brd (
 taxonomic_order INT,
@@ -75,16 +76,17 @@ reviewed BOOLEAN,
 reason VARCHAR(50),
 species_comments VARCHAR);
 
--- OPTIONALADD-ONS BELOW THIS POINT
-                  
--- NOCTURNAL TAGS ON CHECKLIST, request from ebird central
+
+--OPTIONAL THINGS AFTER HERE
+
+-- OPTIONAL NOCTURNAL TAGS ON CHECKLIST, request from ebird central
 --CREATE TABLE noc (
 --sampling_event_identifier VARCHAR(15) UNIQUE NOT NULL PRIMARY KEY,
 --is_noc VARCHAR(9));
                   
 -- OPTIONAL CLICKABLE HTML URL TO EBIRD CHECKLIST (SEE SHINY APP CODE)
 CREATE TABLE url (
-global_unique_identifier VARCHAR(50) UNIQUE NOT NULL PRIMARY KEY REFERENCES obs (global_unique_identifier),
+sampling_event_identifier VARCHAR(50) UNIQUE NOT NULL PRIMARY KEY,
 checklistlink VARCHAR(110));
 
 -- OPTIONAL BLOCK NAMES, request from ebird central
@@ -96,28 +98,29 @@ block_name VARCHAR(100));
                    
 -- add invalid records, request from ebird central
                                    
--- add taxonomy (optional, brings in sort by taxon option or use of 6-letter code as better key for birds)
+-- add taxonomy? (optional, brings in sort by taxon option or use of 6-letter code as better key for birds)
 
 /* 
 This populates the tables with data. Change path to the proper files. In order for the program to be allowed access, you need to find file or folder, then go to: properties, security, Edit, Add "Everyone" as a user, then allow all permissions to Everyone. Second word below is the name of the table we are copying into. These csv files are  created from the eBird download with the R code, except for user which is prepared separately.
  */
 
-COPY BRD FROM 'C:\Users\nicho\Desktop\database\dec6test\brd.csv' DELIMITER ',' CSV HEADER NULL AS 'NA';
+COPY BRD FROM 'C:\Users\nicho\Desktop\Dec2020downloadplusdatabase\brd.csv' DELIMITER ',' CSV HEADER NULL AS 'NA';
 
---make sure userIDs say obsr123 not user123 (they are listed as obsr in the EBD but user on any back-end exports)
-COPY USR FROM 'C:\Users\nicho\Desktop\database\dec6test\usr.csv' DELIMITER ',' CSV HEADER NULL AS 'NA';
+--make sure userIDs say obsr123 not user123 (they are listed as obsr in the EBD but user on some back-end exports)
+COPY USR FROM 'C:\Users\nicho\Desktop\Dec2020downloadplusdatabase\usr.csv' DELIMITER ',' CSV HEADER NULL AS 'NA';
 
-COPY LOC FROM 'C:\Users\nicho\Desktop\database\dec6test\loc.csv' DELIMITER ',' CSV HEADER NULL AS 'NA';
+COPY LOC FROM 'C:\Users\nicho\Desktop\Dec2020downloadplusdatabase\loc.csv' DELIMITER ',' CSV HEADER NULL AS 'NA';
 
-COPY SUB FROM 'C:\Users\nicho\Desktop\database\dec6test\sub.csv' DELIMITER ',' CSV HEADER NULL AS 'NA';
+--optional block name table
+COPY BNM FROM 'C:\Users\nicho\Desktop\Dec2020downloadplusdatabase\bnm.csv' DELIMITER ',' CSV HEADER NULL AS 'NA';
+
+COPY SUB FROM 'C:\Users\nicho\Desktop\Dec2020downloadplusdatabase\sub.csv' DELIMITER ',' CSV HEADER NULL AS 'NA';
+
+--will have to process R script (Shiny folder) before you can add URL
+COPY URL FROM 'C:\Users\nicho\Desktop\Dec2020downloadplusdatabase\url.csv' DELIMITER ',' CSV HEADER NULL AS 'NA';
 
 --obs is the largest table so this next line can take quite a while to load if the file is large               
-COPY OBS FROM 'C:\Users\nicho\Desktop\database\dec6test\obs.csv' DELIMITER ',' CSV HEADER NULL AS 'NA';
+COPY OBS FROM 'C:\Users\nicho\Desktop\Dec2020downloadplusdatabase\obs.csv' DELIMITER ',' CSV HEADER NULL AS 'NA';
 
---COPY NOC FROM 'C:\Users\nicho\Desktop\database\dec5test\noc.csv' DELIMITER ',' CSV HEADER NULL AS 'NA';
-
---will have to process R script before you can add URL, see Shiny code
-COPY URL FROM 'C:\Users\nicho\Desktop\database\dec6test\url.csv' DELIMITER ',' CSV HEADER NULL AS 'NA';
-                   
-COPY BNM FROM 'C:\Users\nicho\Desktop\database\dec6test\bnm.csv' DELIMITER ',' CSV HEADER NULL AS 'NA';
-                   
+--optional nocturnal tag
+--COPY NOC FROM 'C:\Users\nicho\Desktop\Dec2020downloadplusdatabase\noc.csv' DELIMITER ',' CSV HEADER NULL AS 'NA';
